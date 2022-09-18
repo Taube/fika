8cb8a7b26ed1213492237e1693b1990a200e1c26
Leo Taube <10.leo.taube@gmail.com>
2022-09-18 18:42:29 +0200

    små buggar och tid/datum när man får sin beställni

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -207,3 +207,16 @@ window.addEventListener("load", function () {
   phone.addEventListener("blur", () => validatePhone());
   phone.addEventListener("keyup", () => validatePhone());
 });
+
+// this example takes 2 seconds to run
+const start = Date.now();
+
+console.log("starting timer...");
+// expected output: starting timer...
+
+setTimeout(() => {
+  const millis = Date.now() - start;
+
+  console.log(`seconds elapsed = ${Math.floor(millis / 1000)}`);
+  // expected output: seconds elapsed = 2
+}, 2000);
